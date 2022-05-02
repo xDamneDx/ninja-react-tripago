7378a28aa41beaa742fa7616108a2a3fb70f0545
xDamneDx <xdamnedx@gmail.com>
2022-05-02 21:09:07 +0300

    Added useCallback for function in useEffect

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,15 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./TripList.css";
 
 export default function TripList() {
   const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
 
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setTrips(json));
+  const fetchTrips = useCallback(async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setTrips(json);
   }, [url]);
+
+  useEffect(() => {
+    fetchTrips();
+  }, [fetchTrips]);
 
   return (
     <div className="trip-list">
