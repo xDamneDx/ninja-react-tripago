20eaf7a87b3fb6f4074b3cdada581922608928cb
xDamneDx <xdamnedx@gmail.com>
2022-05-02 17:44:56 +0300

    Added fetching data w/ useEffect

---
+++ src/components/TripList.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import "./TripList.css";
+
+export default function TripList() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/trips")
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+  }, []);
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
